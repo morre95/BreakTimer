ccc8218e39b08c82acee84f426b0e960d4b81266
Erik Morén <erik.moren78@gmail.com>
2024-08-08 15:25:20 +0200

    small fixes

--- BreakTimer/MainPage.xaml.cs
+++ BreakTimer/MainPage.xaml.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.Logging;
 using SQLite;
 using System.Diagnostics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
 
 namespace BreakTimer
@@ -9,20 +11,23 @@ namespace BreakTimer
     public partial class MainPage : ContentPage
     {
         private int seconds = 0;
+        private int oldSeconds = 0;
 
         private bool _timeOfDayIsChecked;
         public bool TimeOfDayIsChecked { get { return _timeOfDayIsChecked; } private set { _timeOfDayIsChecked = !value; } }
 
         private TimeTableDatabase Database { get; set; }
 
-        private ILogger<MainPage> Logger { get; set; }
+        private ILogger<MainPage> _logger { get; set; }
+
+        Stopwatch stopwatch;
 
         public MainPage(ILogger<MainPage> logger)
         {
             InitializeComponent();
             Database = new();
 
-            Logger = logger;
+            _logger = logger;
         }
 
         private async void StartTimerClicked(object sender, EventArgs e)
@@ -31,8 +36,8 @@ namespace BreakTimer
             var items = await Database.GetItemsAsync();
             foreach (var item in items)
             {
-                Debug.WriteLine($"#{item.ID}, {item.Seconds}, {item.Duration.ToString()}");
-                Logger.LogDebug($"#{item.ID}, Sec: {item.Seconds}, Duration: {item.Duration.ToString()}, sound: {item.Sound}");
+                Debug.WriteLine($"#{item.ID}, {item.Seconds}, {item.Duration.ToString()}, Item: {JsonSerializer.Serialize(item)}");
+                _logger.LogDebug($"#{item.ID}, Item: {JsonSerializer.Serialize(item)}");
             }
 
             if (sender is Button)
@@ -51,7 +56,7 @@ namespace BreakTimer
                     CheckIfTimeAsync(TimeEntry);
                 }
 
-                int oldSec = seconds;
+                oldSeconds = seconds;
 
                 Image image = new Image
                 {
@@ -65,48 +70,21 @@ namespace BreakTimer
                 UpdateTimeText();
 
 
-                //Implementera denna kod
-                Debug.WriteLine(oldSec + " Sec");
 
-                Stopwatch stopwatch = new Stopwatch();
                 var timer = Dispatcher.CreateTimer();
                 timer.Interval = TimeSpan.FromSeconds(1);
                 timer.Tick += TimerAsync;
-                stopwatch.Start();
+
+                stopwatch = Stopwatch.StartNew();
                 timer.Start();
-
-                /*while (seconds >= 0)
-                {
-                    UpdateTimeText();
-                    seconds--;
-                    await Task.Delay(1000);
-                }
-
-                //await Task.Run(() => PlaySoundAsync(@"Gong_Sound.mp3"));
-                string soundFile = "Gong_Sound.wav";
-                await PlaySoundAsync(soundFile);
-
-                await Database.SaveItemAsync(new()
-                {
-                    Seconds = oldSec,
-                    Sound = soundFile,
-                    Timestamp = DateTime.Now,
-                    Duration = TimeSpan.FromSeconds(oldSec)
-                });
-
-
-                MainView.Children.RemoveAt(0);
-
-                ControllPanel.IsVisible = true;
-                TimeLabel.FontSize = 42;*/
 
             }
         }
 
         private async void TimerAsync(object? sender, EventArgs e)
         {
-            UpdateTimeText();
             seconds--;
+            UpdateTimeText();
 
             if (seconds <= 0 && sender is IDispatcherTimer t) 
             {
@@ -116,17 +94,20 @@ namespace BreakTimer
 
                 await Database.SaveItemAsync(new()
                 {
-                    Seconds = seconds,
+                    Seconds = oldSeconds,
                     Sound = soundFile,
                     Timestamp = DateTime.Now,
-                    Duration = TimeSpan.FromSeconds(seconds)
+                    Duration = TimeSpan.FromSeconds(oldSeconds)
                 });
-
 
                 MainView.Children.RemoveAt(0);
 
                 ControllPanel.IsVisible = true;
                 TimeLabel.FontSize = 42;
+
+                seconds = oldSeconds;
+
+                UpdateTimeText(true);
             }
         }
 
@@ -138,7 +119,8 @@ namespace BreakTimer
                 await CrossMediaManager.Current.Play(stream, MediaManager.Media.MimeType.AudioMp3);
                 await Task.Delay(100);
                 await Task.Delay(CrossMediaManager.Current.Duration);
-                Debug.WriteLine($"Duration: {CrossMediaManager.Current.Duration.ToString()}, Position: {CrossMediaManager.Current.Position.ToString()}, Buffered: {CrossMediaManager.Current.Buffered.ToString()}");
+                //Debug.WriteLine($"Duration: {CrossMediaManager.Current.Duration.ToString()}, Position: {CrossMediaManager.Current.Position.ToString()}, Buffered: {CrossMediaManager.Current.Buffered.ToString()}");
+                _logger.LogInformation($"Duration: {CrossMediaManager.Current.Duration.ToString()}, Position: {CrossMediaManager.Current.Position.ToString()}, Buffered: {CrossMediaManager.Current.Buffered.ToString()}");
             } 
         }
 
@@ -197,7 +179,10 @@ namespace BreakTimer
         {
             if (sender is Entry timeEntry)
             {
-                CheckIfTimeAsync(timeEntry);
+                if (!CheckIfTimeAsync(timeEntry))
+                {
+                    _logger.LogDebug($"'{timeEntry.Text}' is not a volid time");
+                }
             }
         }
 
@@ -210,7 +195,7 @@ namespace BreakTimer
             }
         }
 
-        private void CheckIfTimeAsync(Entry timeEntry)
+        private bool CheckIfTimeAsync(Entry timeEntry)
         {
             if (IsValidTime(timeEntry.Text))
             {
@@ -222,6 +207,7 @@ namespace BreakTimer
                         seconds = Math.Abs((int)diff.TotalSeconds);
                         UpdateTimeText();
                         UpdateInfoTimeText(date);
+                        return true;
                     }
                 }
             }
@@ -231,6 +217,7 @@ namespace BreakTimer
                 if (int.TryParse(secondText, out seconds))
                 {
                     UpdateTimeText(true);
+                    return true;
                 }
             }
             else if (timeEntry.Text.ToLower().EndsWith('m'))
@@ -240,6 +227,7 @@ namespace BreakTimer
                 {
                     seconds = minutes * 60;
                     UpdateTimeText(true);
+                    return true;
                 }
             }
             else if (timeEntry.Text.ToLower().EndsWith('h'))
@@ -249,13 +237,16 @@ namespace BreakTimer
                 {
                     seconds = hours * 60 * 60;
                     UpdateTimeText(true);
+                    return true;
                 }
             }
             else if (!string.IsNullOrEmpty(timeEntry.Text) && timeEntry.Text.Length >= 4)
             {
-                seconds = -1;
+                seconds = 0;
+                _logger.LogDebug($"The text '{timeEntry.Text}' is not a volid time");
                 //await DisplayAlert("Alert", $"'{timeEntry.Text}' is not a vaild time", "OK");
             }
+            return false;
         }
 
         public static bool IsValidTime(string str)
@@ -272,6 +263,8 @@ namespace BreakTimer
                 UpdateTimeText(true);
             }
         }
+
+
     }
 
     public static class Constants
